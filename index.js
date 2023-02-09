e129599a892b10f192ac1dc76e1365154a56e7ca
GilgameshEU <d1vergence@yandex.ru>
2023-02-09 20:51:32 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ const app = express();
 
 const allowedOrigins = ["https://main.d2nb1gulmumrp7.amplifyapp.com", "https://coursebackproject.onrender.com"];
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(router);
-
 app.use(
-  "/login",
   cors({
     origin: function (origin, callback) {
       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
@@ -27,5 +22,9 @@ app.use(
     methods: ["GET", "POST", "DELETE"],
   })
 );
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () => console.log("Server running at port 5000"));
