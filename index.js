5e3da6c64ff1d1e2e69dc15b66854045ded18722
GilgameshEU <d1vergence@yandex.ru>
2023-02-09 20:46:42 +0400

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ dotenv.config();
 const app = express();
 
 const allowedOrigins = ["https://main.d2nb1gulmumrp7.amplifyapp.com", "https://coursebackproject.onrender.com"];
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+
 app.use(
+  "/login",
   cors({
-    credentials: true,
     origin: function (origin, callback) {
       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
         callback(null, true);
@@ -18,17 +23,9 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
+    credentials: true,
     methods: ["GET", "POST", "DELETE"],
   })
 );
-app.use(cookieParser());
-app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(router);
 
 app.listen(5000, () => console.log("Server running at port 5000"));
