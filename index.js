16259f2cc531d0ffb292b61ff778d400677dd061
GilgameshEU <d1vergence@yandex.ru>
2023-02-09 20:58:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,7 @@ import router from "./routes/index.js";
 dotenv.config();
 const app = express();
 
-const allowedOrigins = ["https://main.d2nb1gulmumrp7.amplifyapp.com", "https://coursebackproject.onrender.com"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "DELETE"],
-  })
-);
-
+app.use(cors({ credentials: true, origin: "https://main.d2nb1gulmumrp7.amplifyapp.com", methods: ["GET", "POST", "DELETE"] }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
