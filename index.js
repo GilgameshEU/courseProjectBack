61bb088fbe120d5bf5a0074a24d8e84a7c6e4fd2
GilgameshEU <d1vergence@yandex.ru>
2023-02-09 20:40:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(router);
 
 app.listen(5000, () => console.log("Server running at port 5000"));
